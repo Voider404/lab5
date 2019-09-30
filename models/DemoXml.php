bdd80c9dc27b8160e0c90003b7452648215d802d
Voider_404 <sir4alterno@gmail.com>
2019-09-30 14:16:37 +0300

    upd

--- models/DemoXml.php
+++ models/DemoXml.php
@@ -3,40 +3,14 @@
 namespace app\models;
 
 use Yii;
+use yii\base\Model;
 
 /**
  * This is the model class for table "demoxml".
  *
  * @property int $re
  */
-class DemoXml extends \yii\db\ActiveRecord
+class DemoXml extends Model
 {
-    /**
-     * {@inheritdoc}
-     */
-    public static function tableName()
-    {
-        return 'demoxml';
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['re'], 'required'],
-            [['re'], 'integer'],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            're' => 'Re',
-        ];
-    }
 }
